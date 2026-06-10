3603599b9603224cc0f1f538e8e62cf4f557781b
Jignasa Erthana <jignasa.thakor@gmail.com>
2026-06-10 16:30:19 +0530

    Create nested_dictionary.py

---
+++ Dictionaries/nested_dictionary.py
@@ -0,0 +1,13 @@
+students = {
+    "student1": {
+        "name": "Jignasa",
+        "age": 20
+    },
+    "student2": {
+        "name": "Riya",
+        "age": 21
+    }
+}
+
+print(students)
+print(students["student1"]["name"])
