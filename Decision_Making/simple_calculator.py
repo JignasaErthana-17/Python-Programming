cadafab26d56f8e43a1640b0ecaf1b06c3671cff
Jignasa Erthana <jignasa.thakor@gmail.com>
2026-06-10 16:10:09 +0530

    Create simple_calculator.py

---
+++ Decision_Making/simple_calculator.py
@@ -0,0 +1,20 @@
+num1 = float(input("Enter First Number: "))
+num2 = float(input("Enter Second Number: "))
+
+print("1. Addition")
+print("2. Subtraction")
+print("3. Multiplication")
+print("4. Division")
+
+choice = int(input("Enter Choice: "))
+
+if choice == 1:
+    print("Result =", num1 + num2)
+elif choice == 2:
+    print("Result =", num1 - num2)
+elif choice == 3:
+    print("Result =", num1 * num2)
+elif choice == 4:
+    print("Result =", num1 / num2)
+else:
+    print("Invalid Choice")
