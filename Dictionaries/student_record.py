c53f7595ea38f94229d214470d956dcfb173fc64
Jignasa Erthana <jignasa.thakor@gmail.com>
2026-06-10 16:30:34 +0530

    Create student_record.py

---
+++ Dictionaries/student_record.py
@@ -0,0 +1,10 @@
+student = {}
+
+student["roll_no"] = int(input("Enter Roll No: "))
+student["name"] = input("Enter Name: ")
+student["marks"] = float(input("Enter Marks: "))
+
+print("\nStudent Record")
+print("Roll No:", student["roll_no"])
+print("Name:", student["name"])
+print("Marks:", student["marks"])
