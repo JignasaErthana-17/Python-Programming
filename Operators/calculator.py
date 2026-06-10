25d09ba7259d5f748c77624516f135365a14937d
Jignasa Erthana <jignasa.thakor@gmail.com>
2026-06-10 16:07:18 +0530

    Create calculator.py

---
+++ Operators/calculator.py
@@ -0,0 +1,7 @@
+a = float(input("Enter First Number: "))
+b = float(input("Enter Second Number: "))
+
+print("Addition =", a + b)
+print("Subtraction =", a - b)
+print("Multiplication =", a * b)
+print("Division =", a / b)
